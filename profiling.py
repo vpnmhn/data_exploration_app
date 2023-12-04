6d94be31782ab6e7eacd5db18f99ee38b7725032
NPriyankaDS <nprinka235@gmail.com>
2023-12-04 14:40:31 +0530

    Changes in the header

--- profiling.py
+++ profiling.py
@@ -9,8 +9,8 @@ from streamlit_pandas_profiling import st_profile_report
 def main():
     st.set_page_config(page_title="CSVExplorer",page_icon=":bar_chart:")
     st.title("CSVExplorer:bar_chart:")
-    st.header('Explore a dataset:bar_chart:')
-    st.markdown("This app will help you do data exploration")
+    st.header('Explore a dataset')
+    st.markdown("This app will help you do data exploration!!")
     st.sidebar.header("User input features")
     file = st.sidebar.file_uploader("Files uploader", type=['csv'])
     if file is not None:
